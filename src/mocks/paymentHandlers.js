b54df1aa9635627623af6911a7d2b4d348560c5d
Sangkun-svg <gims4752@gmail.com>
2022-12-28 21:45:42 +0900

    feat : change mock api return value

--- src/mocks/paymentHandlers.js
+++ src/mocks/paymentHandlers.js
@@ -2,7 +2,7 @@ import { rest } from "msw";
 
 export const paymentHandlers = [
   // Handles a POST /payment/complete request
-  rest.post("/payment/complete", null),
-  // Handles a GET /payment request
-  rest.get("/payment", null),
+  rest.get(`/payment/complete`, (req, res, ctx) => {
+    return res(ctx.text("text"));
+  }),
 ];
