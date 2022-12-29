9ea50be667b53f4aacdeeb326f0184060a57ddcf
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2022-12-29 13:41:50 +0900

    Fix Scroll restoration (#51)
    * feat : fix scroll location in payment page
    
    * feat : implement scroll setter component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
