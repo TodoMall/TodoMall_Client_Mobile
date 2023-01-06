93f21be70bda4230d1a51063839d917a73bca02d
Sangkun-svg <gims4752@gmail.com>
2023-01-06 15:47:06 +0900

    design : chaneg img when user item is empty in todobox

--- src/components/main/TodoBox/TodoBox.js
+++ src/components/main/TodoBox/TodoBox.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Loader, BottomNavBar } from "../../global";
+import { Loader, BottomNavBar, ThinText } from "../../global";
 import TodoBoxContent from "./TodoBoxContent";
 import TodoBoxHeader from "./TodoBoxHeader";
 
@@ -59,7 +59,7 @@ const TodoBox = () => {
             <TodoBoxContent plans={plans} check={check} setCheck={setCheck} />
           ) : (
             <TodoBoxEmptyContainer>
-              <TodoBoxEmptyImage src="/images/TodoBoxEmptyImage.svg" />
+              <TodoBoxEmptyImage src="/images/purchase_first.svg" />
               <TodoBoxEmptyWelcome>
                 아직 도전중인 클래스가 없네요!
               </TodoBoxEmptyWelcome>
@@ -69,6 +69,11 @@ const TodoBox = () => {
               <TodoBoxEmptyDescription>
                 여기에서 모아볼 수 있어요.
               </TodoBoxEmptyDescription>
+              <SpeechBubble>
+                <ThinText color="#ffffff" width="auto">
+                  지금 투두몰과 함께 시작해보시는건 어때요?
+                </ThinText>
+              </SpeechBubble>
             </TodoBoxEmptyContainer>
           )}
         </TodoBoxBody>
@@ -78,6 +83,27 @@ const TodoBox = () => {
     </>
   );
 };
+
+const SpeechBubble = styled.div`
+  width: 276px;
+  height: 37px;
+  padding: 8px 20px;
+  gap: 10px;
+  background-color: #6b47fd;
+  border-radius: 14.5px;
+  position: relative;
+  top: 77px;
+  &:after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    border: 19px solid transparent;
+    border-top-color: #6b47fd;
+    border-bottom: 0;
+    margin-left: -20px;
+    margin-bottom: -20px;
+  }
+`;
 
 const TodoBoxBody = styled.div``;
 
@@ -95,7 +121,8 @@ const TodoBoxEmptyContainer = styled.div`
 `;
 
 const TodoBoxEmptyImage = styled.img`
-  width: 90vw;
+  width: 295.11px;
+  height: 230.22px;
   max-width: 450px;
 `;
 
