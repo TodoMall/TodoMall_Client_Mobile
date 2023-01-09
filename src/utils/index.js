a0e78a9b441e7f8e944e45dafadc690ea799b156
Sangkun-svg <gims4752@gmail.com>
2023-01-09 17:08:28 +0900

    refactor: change function name

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 export { default as RedirectByAuthStatus } from "./RedirectByAuthStatus";
 export { default as ScrollToTop } from "./ScrollToTop";
-export { default as SetColorAccordingTime } from "./SetColorAccordingTime";
+export { default as changeColorBasedOnRemainingPeriod } from "./ChangeColorBasedOnRemainingPeriod";
