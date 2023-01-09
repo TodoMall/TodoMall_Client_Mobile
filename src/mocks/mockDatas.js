ddad106133a477edb97b66b1a49891ed3d93cd10
Sangkun-svg <gims4752@gmail.com>
2023-01-09 16:57:24 +0900

    design  : generate mock Todo data for mocking

--- src/mocks/mockDatas.js
+++ src/mocks/mockDatas.js
@@ -1,5 +1,39 @@
 export const mockData = [
   {
+    id: "0",
+    productId: "02d1c4f7-ea1a-4d2f-bfae-92bc73efe6b7",
+    title: "슬기로운 투두몰 생활",
+    status: false,
+    icon: "https://todomall-content.s3.ap-northeast-2.amazonaws.com/product_emoji/CompositingClassIcon_Person.svg",
+    sessions: [
+      {
+        id: "8871905b-d7d4-4bdd-b1f9-aebc2cc94a2d100",
+        title: "튜토리얼_0",
+        status: false,
+        startDate: "2023-01-07T15:11:45.404Z",
+        expireDate: "2023-01-09T15:11:45.404Z",
+        orderBy: 0,
+        todos: [
+          {
+            id: "5b19d4be-962f-45fa-8380-08df71d7c10d",
+            title: "투두를 따라하고 성장하기",
+            status: true,
+          },
+          {
+            id: "c5214397-eb09-4b34-86c1-5bd6c883a265",
+            title: "미션을 인증하고 성장하기",
+            status: true,
+          },
+          {
+            id: "c396d2aa-0624-407c-9e15-c804e7fba6ec",
+            title: "(iOS 유저) 앱처럼 투두몰 접속하기",
+            status: true,
+          },
+        ],
+      },
+    ],
+  },
+  {
     id: "1",
     productId: "02d1c4f7-ea1a-4d2f-bfae-92bc73efe6b7",
     title: "슬기로운 투두몰 생활",
