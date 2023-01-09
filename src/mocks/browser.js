7360d2e198a9a434f0318d5a73a511b217c9eaf7
Sangkun-svg <gims4752@gmail.com>
2023-01-09 15:52:29 +0900

    feat : implement mock api for local test

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,5 +1,6 @@
 import { setupWorker } from "msw";
 import { paymentHandlers } from "./paymentHandlers";
+import { userTodoHandler } from "./userTodoHandler";
 
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...paymentHandlers);
+export const worker = setupWorker(...paymentHandlers, ...userTodoHandler);
