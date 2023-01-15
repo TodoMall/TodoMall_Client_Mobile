07647c9fb99d459fb637b0a819af05425eaa6b4d
Sangkun-svg <gims4752@gmail.com>
2023-01-15 17:24:46 +0900

    design : change modal style

--- src/components/settings/Modals.js
+++ src/components/settings/Modals.js
@@ -16,6 +16,7 @@ const Modals = ({
   const EMAIL = localStorage.getItem("email");
   const navigate = useNavigate();
   const access_token = localStorage.getItem("access");
+
   const handleLogout = () => {
     axios
       .get(
@@ -61,25 +62,43 @@ const Modals = ({
         aria-labelledby="modal-title"
         open={visibleLogout}
         onClose={closeHandlerLogout}
-        width={MAX_WIDTH}
+        width={"calc(100% - 32px)"}
+        style={{ borderRadius: "32px" }}
       >
         <Container>
           <ModalImage src="/images/modal_image.svg" />
           <ModalTitle>정말 로그아웃 하실 건가요?</ModalTitle>
           <ModalDetail>
-            지금 로그아웃 하면, 이후에 서비스를 이용할 때 다시 로그인 해야 해요.
+            이후 서비스를 이용할 시 로그인 해야 하고 중요한 알림을 받을 수
+            없어요.
           </ModalDetail>
           <ModalButton>
-            <Button title="로그아웃" width={45} onClick={handleLogout} />
-            <CancelButton onClick={closeHandlerLogout}>취소</CancelButton>
+            <Button
+              title="로그아웃"
+              width={45}
+              borderRadius="20px"
+              onClick={handleLogout}
+              color="#ffffff"
+            />
+            <Button
+              title="취소"
+              onClick={closeHandlerLogout}
+              width={45}
+              borderRadius="20px"
+              background="#FFFFFF"
+              border="#c0c0c0"
+              color="#444444"
+            />
           </ModalButton>
         </Container>
       </Modal>
+
       <Modal
         aria-labelledby="modal-titl"
         open={visibleDelete}
         onClose={closeHandlerDelete}
-        width={MAX_WIDTH}
+        width={"calc(100% - 32px)"}
+        style={{ borderRadius: "32px" }}
       >
         <Container>
           <ModalImage src="/images/modal_image.svg" />
@@ -90,12 +109,21 @@ const Modals = ({
           </ModalDetail>
           <ModalButton>
             <Button
-              color="#F65050"
               title="탈퇴하기"
               width={45}
               onClick={handleDelete}
+              color="#ffffff"
+              background="#FF4D4F"
+              border="none"
             />
-            <CancelButton onClick={closeHandlerDelete}>취소</CancelButton>
+            <Button
+              title="취소"
+              onClick={closeHandlerDelete}
+              width={45}
+              background="#FFFFFF"
+              border="#c0c0c0"
+              color="#444444"
+            />
           </ModalButton>
         </Container>
       </Modal>
@@ -112,19 +140,22 @@ const Container = styled.div`
 
 const ModalTitle = styled.p`
   font-weight: 700;
-  font-size: 22px;
+  font-size: 20px;
+  line-height: 32px;
   text-align: center;
-  color: #000000;
-  margin-top: 20px;
+  color: #222222;
+  margin-top: 16px;
 `;
 
 const ModalDetail = styled.p`
-  font-weight: 500;
-  font-size: 18px;
+  width: 224px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 21px;
   text-align: center;
-  margin-top: 20px;
-  padding: 0 20px;
-  color: #888888;
+  margin-top: 8px;
+  color: #444444;
+  word-break: keep-all;
 `;
 
 const ModalButton = styled.div`
@@ -133,7 +164,7 @@ const ModalButton = styled.div`
   align-items: center;
   margin-bottom: 20px;
   gap: 10px;
-  width: 100%;
+  width: calc(100% - 29px);
 `;
 
 const CancelButton = styled.div`
