3d2d33aaf5a545c0310fcafc5438a1d064694b25
Sangkun-svg <gims4752@gmail.com>
2023-01-17 23:06:02 +0900

    feat : set color only progress todo

--- src/utils/SelectCheckBoxBasedOnPeriod.js
+++ src/utils/SelectCheckBoxBasedOnPeriod.js
@@ -1,10 +1,4 @@
-/* parameters : (duration : number ,  : boolean , isClear : boolean) */
-// 모든 투두 완료 & 인증만 남은 경우 "rgba(24, 144, 255, 1)" : "rgba(24, 144, 255, 0.2)";
-// 남은 투두가 있는 상황 : 가장 가까운 투두만 expireDate에 맞는 컬러
-// 끝나지 않은 투두 && 완료되지 않은 2번째 투두 : 기존과 같이
-// 남은 투두가 있는 상황 && 완료된 투두 : 기존과 같이 완료된 투두
-
-const selectCheckBoxBasedOnPeriod = (period, isDone, todos) => {
+const selectCheckBoxBasedOnPeriod = (period, isDone, isProgress, todos) => {
   let icon;
   const unfinishedTodos = todos?.filter((el) => el.status === false);
 
@@ -14,6 +8,9 @@ const selectCheckBoxBasedOnPeriod = (period, isDone, todos) => {
       break;
     case isDone:
       icon = "TodoBoxCheckBoxOn";
+      break;
+    case !isProgress:
+      icon = "TodoBoxCheckBoxOff";
       break;
     case period >= 4:
       icon = "checkbox_green";
