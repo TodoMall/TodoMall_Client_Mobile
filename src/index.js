60ce4176bab22e7ece0a300b5cff23e4911a83e3
Sangkun-svg <gims4752@gmail.com>
2023-01-19 12:39:52 +0900

    build : setup apollo

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { worker } from "./mocks/browser";
 import { ScrollToTop } from "./utils";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 if (process.env.REACT_APP_NODE_ENV === "development") {
   worker.start({
     onUnhandledRequest: "bypass",
   });
 }
+const SERVER_API_URL = process.env.SERVER_API_URL;
+
+const client = new ApolloClient({
+  uri: SERVER_API_URL,
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ScrollToTop />
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>
 );
 
