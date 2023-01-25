60e0d2899401344eff3889c7ac08b636d2e29b89
Sangkun-svg <gims4752@gmail.com>
2023-01-25 20:15:09 +0900

    design : delete unused contants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export { Fail, Success, paymentResultData, PaymentMethods } from "./payment";
+export { paymentResultData, PaymentMethods, ORDER_STATE } from "./payment";
 export { MAX_WIDTH, VERSION, COLOR, USER_TYPE } from "./common";
 export { baseApiUrl } from "./env";
