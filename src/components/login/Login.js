46650dbb4b83450fa295f992e80eb58be13e6767
Sangkun-svg <gims4752@gmail.com>
2023-01-27 17:24:57 +0900

    refactor : change delete method of localstorage

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -57,7 +57,7 @@ const Login = () => {
   };
 
   const handleGuest = () => {
-    handleStorage();
+    localStorage.clear();
     localStorage.setItem("access", USER_TYPE.GUEST);
     navigate("/todomall");
   };
