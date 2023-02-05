9e9fb179a1d93dcec8d313d8752fbb2a77646a5f
Sangkun-svg <gims4752@gmail.com>
2023-02-05 19:40:10 +0900

    design : delte comments

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,11 +1,8 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 const useToggle = (initialState = false) => {
   const [state, setState] = useState(initialState);
 
-  // Define and memorize toggler function in case we pass down the component,
-  // This function change the boolean value to it's opposite value
-  //   const toggle = useCallback(() => setState((state) => !state), []);
   const toggle = () => setState((state) => !state);
 
   return [state, setState, toggle];
