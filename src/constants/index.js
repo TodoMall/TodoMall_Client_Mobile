6d532ef036629c34fce289aa6b9307aa19990b6d
Sangkun-svg <gims4752@gmail.com>
2023-02-06 17:34:41 +0900

    feat : management path to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,4 @@ export { MAX_WIDTH, VERSION, GNB_IMAGE, USER_TYPE } from "./common";
 export { baseApiUrl, API_ENDPOINT, isProd } from "./env";
 export { COLOR } from "./color";
 export { LOCAL_STORAGE_KEYS } from "./localstorage";
+export { PATH } from "./path";
---
+++ src/constants/path.js
@@ -0,0 +1,6 @@
+export const PATH = {
+  SERVICE: "/service",
+  PERSONAL: "/personal",
+  TODOBOX: "/todobox",
+  TODOMALL: "/todomall",
+};
