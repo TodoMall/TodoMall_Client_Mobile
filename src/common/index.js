8d51830e964203448e386ebb1f95dc6b082718ab
Sangkun-svg <gims4752@gmail.com>
2023-02-08 19:05:02 +0900

    feat : implement Loader

--- src/common/index.js
+++ src/common/index.js
@@ -1,2 +1,3 @@
 export { default as CheckItem } from "./components/CheckItem";
 export { default as DetailItem } from "./components/DetailItem";
+export { default as Loader } from "./components/Loader";
