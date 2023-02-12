57b98c6b7a8d72db1144ffa2fa6037138caf86f5
Sangkun-svg <gims4752@gmail.com>
2023-02-12 18:55:15 +0900

    feat : re-build folder structure

---
+++ src/constants/spacingSize.js
@@ -0,0 +1,10 @@
+export const SPACING_SIZE = {
+  spacing_50: "2px",
+  spacing_100: "4px",
+  spacing_200: "8px",
+  spacing_300: "12px",
+  spacing_400: "16px", // default
+  spacing_500: "20px",
+  spacing_600: "24px",
+  spacing_700: "32px",
+};
