83bf60442dbb1e80d9dc67c61f589b27630c0418
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2023-02-13 13:47:48 +0900

    [CBT-89] 폴더 구조를 개편한다 (#68)
    * design : change compony address (#67)
    
    * refactor : delete usused file
    
    * feat : re-build folder structure
    
    * refactor : delete unused codes
    
    * refactor : delete usused image files
    
    * refactor : change file extensions

---
+++ src/constants/iamportPgInfo.js
@@ -0,0 +1,38 @@
+export const IamportPaymentGateInfo = [
+  {
+    id: 1,
+    name: "card",
+    pg: "uplus.tlgdacomxpay",
+    MID: "tlgdacomxpay",
+    pay_method: "card",
+    iconPath: "/images/payment/cardPayIcon.svg",
+    description: "카드결제",
+  },
+  {
+    id: 2,
+    name: "trans",
+    pg: "uplus.tlgdacomxpay",
+    MID: "tlgdacomxpay",
+    pay_method: "trans",
+    iconPath: "/images/payment/accountTransferIcon.svg",
+    description: "실시간 계좌이체",
+  },
+  {
+    id: 3,
+    name: "tosspay",
+    pg: "tosspay.tosstest",
+    MID: "tosstest",
+    pay_method: "card",
+    iconPath: "/images/payment/tossPayIcon.png",
+    description: "토스페이",
+  },
+  {
+    id: 4,
+    name: "kakaopay",
+    pg: "kakaopay.TC0ONETIME",
+    MID: "TC0ONETIME",
+    pay_method: "card",
+    iconPath: "/images/payment/kakaoPayIcon.svg",
+    description: "카카오페이",
+  },
+];
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,2 @@
-export { default as changeColorBasedOnRemainingPeriod } from "./ChangeColorBasedOnRemainingPeriod";
 export { default as RedirectByAuthStatus } from "./RedirectByAuthStatus";
 export { default as ScrollToTop } from "./ScrollToTop";
-export { default as selectCheckBoxBasedOnPeriod } from "./SelectCheckBoxBasedOnPeriod";
-export { default as setProductStatus } from "./setProductStatus";
-export { default as productFilter } from "./productFilter";
-export { default as useModal } from "./useModal";
