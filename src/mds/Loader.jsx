ae5ccd936305e610ec3bab1b956fc349950d3a96
Sangkun-svg <gims4752@gmail.com>
2023-02-15 00:54:42 +0900

    feat : implement Loader component using MUI

---
+++ src/mds/Loader.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+const Loader = () => {
+  return <CircularProgress />;
+};
+
+export default Loader;
+
+const Container = styled.div``;
