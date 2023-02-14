c37e80e94c6421dc9ba1e91eff9a2ecab78169f4
Sangkun-svg <gims4752@gmail.com>
2023-02-14 14:40:37 +0900

    feat : implement DockBar demo version

--- src/mds/icon/index.js
+++ src/mds/icon/index.js
@@ -1,3 +1,6 @@
 export { default as CheckIcon } from "./CheckIcon";
 export { default as ArrowIcon } from "./ArrowIcon";
 export { default as RadioIcon } from "./RadioIcon";
+export { default as EducationIcon } from "./EducationIcon";
+export { default as MyPageIcon } from "./MyPageIcon";
+export { default as StoreIcon } from "./StoreIcon";
