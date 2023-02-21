15965e97851d1a9ff358d17b54781a13b148582d
mo-zza <jaewooha@devsisters.com>
2023-02-21 13:07:19 +0900

    feat: introduce sign in provider

---
+++ src/apollo/domain/member/member.mutation.js
@@ -0,0 +1,13 @@
+import gql from "graphql-tag";
+
+export const signInWithKakao = gql`
+    mutation signInWithKakao($data: String!) {
+        signInWithKakao(data: $data)
+    }
+`;
+
+export const signInWithApple = gql`
+    mutation signInWithApple($data: String!) {
+        signInWithApple(data: $data)
+    }
+`
