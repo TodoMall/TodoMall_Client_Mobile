133dbf2ed1e54e6bd1899dd40d44d345b4cffb0c
mo-zza <jaewooha@devsisters.com>
2023-02-21 13:07:34 +0900

    feat: implement custom login hook

---
+++ src/domain/member/hooks/useLogin.js
@@ -0,0 +1,28 @@
+import { useMutation } from "@apollo/client";
+
+import { signInWithKakao, signInWithApple } from "../../../apollo/domain/member";
+import { PROVIDERS } from "../../../constants/providers";
+
+export default function useLogin(provider = PROVIDERS.KAKAO) {
+  const [signIn, { data, loading, error }] = useMutation(
+      getSignInMutation(provider),
+    {
+      variables: {
+        data: "sign in data",
+      },
+    }
+  );
+
+  return { signIn, data, loading, error };
+}
+
+const getSignInMutation = (provider) => {
+  switch (provider) {
+    case PROVIDERS.KAKAO:
+      return signInWithKakao;
+    case PROVIDERS.APPLE:
+      return signInWithApple;
+    default:
+      return signInWithKakao;
+  }
+}
