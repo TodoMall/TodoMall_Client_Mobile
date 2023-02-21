b5a4873391cdb90c378eb3124cfc0ae673901a05
mo-zza <jaewooha@devsisters.com>
2023-02-21 13:06:34 +0900

    feat: implement custom apollo provider

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import styled from "styled-components";
 
 // FIXME : should be deleted
 import TestPage from "./pages/test";
+import { CustomApolloProvider } from "./apollo/link";
+import LoginPage from "./pages/LoginPage";
+import { PATH } from "./constants";
 
 function App() {
+
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<TestPage />} />
-      </Routes>
-    </Container>
+      <CustomApolloProvider>
+        <Container>
+          <Routes>
+            <Route path={PATH.MAIN} element={<TestPage />} />
+            <Route path={PATH.LOGIN} element={<LoginPage />} />
+          </Routes>
+        </Container>
+      </CustomApolloProvider>
   );
 }
 
---
+++ src/apollo/link/CustomApolloProvider.jsx
@@ -0,0 +1,12 @@
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const CustomApolloProvider = ({children = null}) => {
+  const client = new ApolloClient({
+    uri: process.env.REACT_APP_TODO_MALL_API_ENDPOINT,
+    cache: new InMemoryCache()
+  })
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
+
+export default CustomApolloProvider;
