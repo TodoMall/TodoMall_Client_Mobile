5e5e03c98f1063460b639c88d3fb40091e724705
mo-zza <jaewooha@devsisters.com>
2023-02-21 16:13:18 +0900

    feat: introduce brand logo with text image

---
+++ src/mds/image/BrandLogoIWithTextImage.jsx
@@ -0,0 +1,5 @@
+const BrandLogoIWithTextImage = () => {
+  return <img width={328} height={100} src="/image/brand_logo_white_with_text.png" alt="" />
+}
+
+export default BrandLogoIWithTextImage;
--- src/mds/image/index.js
+++ src/mds/image/index.js
@@ -2,3 +2,4 @@ export { default as BrandLogoLikeBubbleImage } from "./BrandLogoLikeBubbleImage"
 export { default as BrandLogoList } from "./BrandLogoList";
 export { default as RocketImage } from "./RocketImage";
 export { default as BookClipImage } from "./BookClipImage";
+export { default as BrandLogoIWithTextImage } from "./BrandLogoIWithTextImage";
