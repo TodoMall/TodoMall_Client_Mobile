4f5b396604febacaa160be649280abef86740b49
mo-zza <jaewooha@devsisters.com>
2023-02-21 11:47:12 +0900

    feat: implement landing page asset components

---
+++ src/mds/image/BrandLogoLikeBubbleImage.jsx
@@ -0,0 +1,7 @@
+const BrandLogoLikeBubbleImage = () => {
+  return (
+      <img width="351.55px" height="280" src="/image/brand_logo_like_bubble.png" alt="" />
+  )
+}
+
+export default BrandLogoLikeBubbleImage;
