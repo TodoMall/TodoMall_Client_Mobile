c21ccde044dc81c86b2a89089f37104915d812e9
mo-zza <jaewooha@devsisters.com>
2023-02-22 11:30:54 +0900

    feat: introduce login environment

---
+++ src/constants/login.js
@@ -0,0 +1,3 @@
+export const KAKAO_ENDPOINT = 'https://kauth.kakao.com/oauth/authorize';
+export const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID || "";
+export const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI || "";
