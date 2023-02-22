aa5e3a3fb9e7bdbc1d1f080b17c3bacc8825acc0
mo-zza <jaewooha@devsisters.com>
2023-02-22 14:02:36 +0900

    feat: implement apple login

--- src/domain/member/components/AppleLoginButton.jsx
+++ src/domain/member/components/AppleLoginButton.jsx
@@ -1,23 +1,41 @@
 import styled from "styled-components";
 
-import { useLogin } from "../hooks";
-import { PROVIDERS } from "../../../constants/providers";
 import { BasicButton } from "../../../mds/button";
-import { COLOR, FONT_STYLE } from "../../../constants";
+import {
+  APPLE_CLIENT_ID,
+  APPLE_SCOPE,
+  COLOR,
+  FONT_STYLE, REDIRECT_URI
+} from "../../../constants";
 
 const AppleLoginButton = () => {
-  const { signIn, data, loading, error } = useLogin(PROVIDERS.APPLE);
+  const initApple = () => {
+    window.AppleID.auth.init({
+      clientId: APPLE_CLIENT_ID,
+      scope: APPLE_SCOPE,
+      redirectURI: REDIRECT_URI,
+      state: "origin:web",
+      usePopup: true
+    })
+  }
+  const signInApple = async () => {
+    const response = await window.AppleID.auth.signIn();
+    // TODO: response에 있는 id_token, email, name을 서버로 보낸다.
+  }
 
   const onClickLoginButton = () => {
-    signIn();
+    initApple();
+    signInApple();
   }
 
   return (
+      <>
       <BasicButton margin="0" width="22.375rem" backgroundColor={COLOR.WHITE} fontColor="#222222" onClick={onClickLoginButton}>
         <ButtonText>
           Apple로 계속하기
         </ButtonText>
       </BasicButton>
+      </>
   )
 }
 
