f9a24c0fe6b1d016eac2e5992162612e05655ef1
mo-zza <jaewooha@devsisters.com>
2023-02-23 17:05:15 +0900

    feat: register signin page and agreement page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,3 +9,5 @@ export { default as TermsPage } from "./TermsPage";
 export { default as AgreementPersonalPage } from "./AgreementPersonalPage";
 export { default as SettingPersonalPage } from "./SettingPersonalPage";
 export { default as TermOfServicePage } from "./TermOfServicePage";
+export { default as AgreementPage } from "./AgreementPage";
+export { default as LoginPage } from "./LoginPage";
