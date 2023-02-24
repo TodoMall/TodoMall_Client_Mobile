d2de473518f9a44bce55df7dc202a4856442d17b
Sangkun-svg <gims4752@gmail.com>
2023-02-24 15:05:25 +0900

    build : move index files location

---
+++ src/domain/store/components/index.js
@@ -0,0 +1,3 @@
+export { default as SearchBar } from "./SearchBar";
+export { default as ClassBox } from "./ClassBox";
+export { default as SessionCard } from "./SessionCard";
---
+++ src/mds/layout/mobile/index.js
@@ -0,0 +1,1 @@
+export { default as GlobalNavBar } from "./GlobalNavBar";
