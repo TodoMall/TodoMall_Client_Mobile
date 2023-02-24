27685735565d31883074f621b60964d01dad68aa
Sangkun-svg <gims4752@gmail.com>
2023-02-24 14:57:55 +0900

    design : add icons

---
+++ src/mds/icon/StoreIcon.jsx
@@ -0,0 +1,37 @@
+const StoreIcon = ({ backgroundColor }) => {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g id="0068036a80e8ad549611b5a21f1b4f2b">
+                <g
+                    id="5d9ba57d4f7a13a8f26549090902f4ee"
+                    clipPath="url(#clip0_2153_36488)"
+                >
+                    <path
+                        id="d4a77e6c04bbf7a28c74aea505fd65dc"
+                        d="M16.9 22.0504H7.09999C5.18999 22.0504 3.58999 20.5904 3.41999 18.6904L2.46999 8.42043C2.39999 7.63043 3.01999 6.94043 3.81999 6.94043H20.2C21 6.94043 21.62 7.62043 21.55 8.42043L20.6 18.6904C20.42 20.5904 18.83 22.0504 16.92 22.0504H16.9Z"
+                        fill={backgroundColor}
+                    ></path>
+                    <path
+                        id="0e9362b29a1ad2aa25d6cfef326d522f"
+                        d="M12.05 11.8996C14.6292 11.8996 16.72 9.80874 16.72 7.22957C16.72 4.6504 14.6292 2.55957 12.05 2.55957C9.47084 2.55957 7.38 4.6504 7.38 7.22957C7.38 9.80874 9.47084 11.8996 12.05 11.8996Z"
+                        strokeWidth="2"
+                        strokeMiterlimit="10"
+                        stroke={backgroundColor}
+                    ></path>
+                </g>
+            </g>
+            <defs>
+                <clipPath id="clip0_2153_36488">
+                    <rect width="24" height="24" fill="white"></rect>
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
+export default StoreIcon;
