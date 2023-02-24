698ebd3681df5f1f524ab286971ac5afaf531f88
mo-zza <jaewooha@devsisters.com>
2023-02-24 14:27:49 +0900

    feat: implement navigator button in agreement button box

--- src/domain/auth/components/AgreementButtonBox.jsx
+++ src/domain/auth/components/AgreementButtonBox.jsx
@@ -11,6 +11,7 @@ import AgreementButtonItem from "./AgreementButtonItem";
 const AgreementButtonBox = () => {
     const navigator = useNavigate();
     const [isAllSelected, setIsAllSelected] = useState(false);
+    const [isRequiredSelectedCount, setIsRequiredSelectedCount] = useState(0);
 
     const agreementList = [
         {
@@ -44,15 +45,49 @@ const AgreementButtonBox = () => {
         setIsAllSelected(!isAllSelected);
     };
 
+    const isAllAgreed = () => {
+        if (isAllSelected) {
+            return true;
+        }
+
+        if (
+            isRequiredSelectedCount ===
+            agreementList.filter(item => item.required).length
+        ) {
+            return true;
+        }
+    };
+
+    const onClickNextButton = () => {
+        if (isAllAgreed() === true) {
+            navigator(PATH.MYCOURSE);
+        }
+    };
+
     const agreementItems = agreementList.map((item, index) => {
         const [isAgreed, setIsAgreed] = useState(false);
 
         const onClickAgreementButton = () => {
-            setIsAgreed(!isAgreed);
+            const value = !isAgreed;
+            setIsAgreed(value);
+
+            if (item.required && value === true) {
+                setIsRequiredSelectedCount(isRequiredSelectedCount + 1);
+            } else if (item.required && value === false) {
+                setIsRequiredSelectedCount(isRequiredSelectedCount - 1);
+            }
         };
 
         useEffect(() => {
-            setIsAgreed(isAllSelected);
+            if (isAllSelected) {
+                setIsAgreed(true);
+                setIsRequiredSelectedCount(
+                    agreementList.filter(item => item.required).length
+                );
+            } else {
+                setIsAgreed(false);
+                setIsRequiredSelectedCount(0);
+            }
         }, [isAllSelected]);
 
         return (
@@ -83,8 +118,10 @@ const AgreementButtonBox = () => {
             </AllAgreementContainer>
             {agreementItems}
             <BasicButton
-                backgroundColor={COLOR.BRAND_COLOR}
-                onClick={() => navigator(PATH.MYCOURSE)}
+                backgroundColor={
+                    isAllAgreed() === true ? COLOR.BRAND_COLOR : COLOR.GRAY200
+                }
+                onClick={onClickNextButton}
             >
                 <BodyL fontColor={COLOR.WHITE}>동의합니다</BodyL>
             </BasicButton>
