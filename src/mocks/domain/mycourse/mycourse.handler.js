f01e1c390a87dc2c7a2c48d84bf8716ab3865df6
Sangkun-svg <gims4752@gmail.com>
2023-02-26 20:49:14 +0900

    feat : divide function and mock data in mocking function

--- src/mocks/domain/mycourse/mycourse.handler.js
+++ src/mocks/domain/mycourse/mycourse.handler.js
@@ -1,48 +1,9 @@
 import { graphql } from "msw";
 
+import { promotionList } from "./mycourse.data";
+
 export const myCourseHandlers = [
     graphql.query("getAllPromotion", (req, res, ctx) => {
-        return res(
-            ctx.data({
-                getAllPromotion: [
-                    {
-                        id: "1",
-                        title: "title 1",
-                        products: {
-                            id: "product id 1",
-                            thumbnailUrl: "/image/rocket.png",
-                            discountPrice: 10,
-                            discountPercent: 20000,
-                            subDescription:
-                                "서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.",
-                        },
-                    },
-                    {
-                        id: "2",
-                        title: "title 2",
-                        products: {
-                            id: "product id 2",
-                            thumbnailUrl: "/image/rocket.png",
-                            discountPrice: 10,
-                            discountPercent: 20000,
-                            subDescription:
-                                "서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.",
-                        },
-                    },
-                    {
-                        id: "3",
-                        title: "title 3",
-                        products: {
-                            id: "product id 3",
-                            thumbnailUrl: "/image/rocket.png",
-                            discountPrice: 10,
-                            discountPercent: 20000,
-                            subDescription:
-                                "서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.서울 사이버 대학을 다니고 나의 성공 시대 시작됐다.",
-                        },
-                    },
-                ],
-            })
-        );
+        return res(ctx.data(promotionList));
     }),
 ];
