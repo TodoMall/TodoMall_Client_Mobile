6c6987caa1f57e819207ff52ef39ff2bb7f74e7a
Sangkun-svg <gims4752@gmail.com>
2023-03-15 14:02:24 +0900

    feat : implement custom dateFormat hooks

--- src/hooks/useQueryString.js
+++ src/hooks/useQueryString.js
@@ -1,5 +1,28 @@
 import { useLocation } from "react-router-dom";
 
+/*
+FIXME : 확장성 고려
+import { useLocation } from "react-router-dom";
+
+const useQueryString = input => {
+    const result = [];
+    const { search } = useLocation();
+    const queryString = new URLSearchParams(search);
+    if (Array.isArray(input)) {
+        input.forEach(el => {
+            result.push(queryString.get(el));
+        });
+    }
+
+    if (!Array.isArray(input)) {
+        result.push(queryString.get(input));
+    }
+
+    return result;
+};
+
+*/
+
 const useQueryString = target => {
     const { search } = useLocation();
 
