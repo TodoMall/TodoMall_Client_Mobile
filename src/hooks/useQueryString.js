01b2ebe6bce3d3321a0de8d6cd594294daaaea70
Sangkun-svg <gims4752@gmail.com>
2023-03-17 03:02:41 +0900

    refactor : get parameter using args

--- src/hooks/useQueryString.js
+++ src/hooks/useQueryString.js
@@ -1,35 +1,19 @@
 import { useLocation } from "react-router-dom";
 
-/*
-FIXME : 확장성 고려
-import { useLocation } from "react-router-dom";
-
-const useQueryString = input => {
+const useQueryString = (...args) => {
     const result = [];
     const { search } = useLocation();
     const queryString = new URLSearchParams(search);
-    if (Array.isArray(input)) {
-        input.forEach(el => {
+    if (Array.isArray(args)) {
+        args.forEach(el => {
             result.push(queryString.get(el));
         });
     }
 
-    if (!Array.isArray(input)) {
-        result.push(queryString.get(input));
+    if (!Array.isArray(args)) {
+        result.push(queryString.get(args));
     }
-
     return result;
-};
-
-*/
-
-const useQueryString = target => {
-    const { search } = useLocation();
-
-    const queryString = new URLSearchParams(search);
-    const query = queryString.get(target);
-
-    return query;
 };
 
 export default useQueryString;
