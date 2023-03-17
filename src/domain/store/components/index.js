f81222540b1851f31be68d9a02cbc5b6e39bb5c6
Sangkun-svg <gims4752@gmail.com>
2023-03-17 17:05:36 +0900

    refactor : delete unused files

--- src/domain/store/components/index.js
+++ src/domain/store/components/index.js
@@ -1,11 +1,9 @@
-export { default as SearchBar } from "./SearchBar";
 export { default as ClassBox } from "./ClassBox";
 export { default as DiscountedClassList } from "./DiscountedClassList";
 export { default as RecommendCategoryClassList } from "./RecommendCategoryClassList";
 export { default as RecommendCard } from "./RecommendCard";
 export { default as RecommendCardList } from "./RecommendCardList";
 export { default as CategoryClassList } from "./CategoryClassList";
-// StoreDetailPage
 export { default as CourseIntroCardList } from "./CourseIntroCardList";
 export { default as CourseAmountTicket } from "./CourseAmountTicket";
 export { default as OurGoalsDescription } from "./OurGoalsDescription";
